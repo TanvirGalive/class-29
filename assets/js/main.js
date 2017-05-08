33a080e6c50b48acc72223d302366421620e2559
TanvirGalive <galivetanvir@gmail.com>
2017-05-08 19:10:36 +0600

    add a new site

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $(".projects-titles li").on('click', function() {
+            
+            $(".projects-titles li").removeClass("active");
+            $(this).addClass("active");
+            
+            
+            var selector = $(this).attr('data-filter');
+            $(".projects-list").isotope({
+                filter:selector
+            });
+        });
+
+    });
+
+
+    jQuery(window).load(function(){
+        jQuery(".projects-list").isotope();
+        
+    });
+
+
+}(jQuery));	
